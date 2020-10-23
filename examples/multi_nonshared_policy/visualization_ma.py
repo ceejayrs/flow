3fb726fa22ab2354e177b29d57311bc5a565c845
ceejayrs <cjrsantos529@gmail.com>
2020-10-24 06:34:46 +0800

    visualization for multiagent

--- examples/multi_nonshared_policy/visualization_ma.py
+++ examples/multi_nonshared_policy/visualization_ma.py
@@ -129,8 +129,8 @@ def replay(env, env_params, agent, rets, ignore_policy):
         if multiagent and done['__all__']:
             break
     if multiagent:
-            for key in rets.keys():
-            rets[key].append(ret[key])
+        for key in rets.keys():
+        rets[key].append(ret[key])
     else:
         rets.append(ret)
 
