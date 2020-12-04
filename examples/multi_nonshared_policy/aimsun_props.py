88f083f0afc9476b7c9632dbabd491f624114df1
Carissa Santos <cjrsantos529@gmail.com>
2020-12-04 22:32:32 +0800

    fix bug

--- examples/multi_nonshared_policy/aimsun_props.py
+++ examples/multi_nonshared_policy/aimsun_props.py
@@ -1,6 +1,6 @@
 import csv
 import sys
-sys.path.append('/home/damian/anaconda3/envs/aimsun_flow/lib/python2.7/site-packages')
+sys.path.append('/home/cjrsantos/anaconda3/envs/aimsun_flow/lib/python2.7/site-packages')
 import numpy as np
 import pandas as pd
 import json
@@ -62,11 +62,11 @@ class Export_Params:
         self.rep_name = str(rep_name) + '_' + str(node_id) + '.csv'
         self.fieldnames = ['time', 'node_id', 'delay_time','action']
 
-    def export_delay_action(self, node_id, delay, action_list, util_list, r_queue, time, timeSta):
+    def export_delay_action(self, node_id, rep_seed, delay, action_list, util_list, r_queue, time, timeSta):
         time = time
         timeSta = timeSta
         ave_app_delay = delay
-        data_list = [time, node_id, delay, r_queue]
+        data_list = [rep_seed, time, node_id, delay, r_queue]
 
         for util in util_list:
             data_list.append(util)
