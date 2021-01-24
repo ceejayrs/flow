021b5fcb3d2922d3d07b29f27719f419de0a5418
Ceejay Santos <cjrsantos529@gmail.com>
2021-01-24 18:05:51 +0800

    aimsun fix

--- examples/aimsun/single_light/debug_export_pt.py
+++ examples/aimsun/single_light/debug_export_pt.py
@@ -4,11 +4,11 @@ import csv
 import PyANGKernel as gk
 #import PyANGConsole as cs
 from datetime import datetime
-sys.path.append('/home/cjrsantos/anaconda3/envs/aimsun_flow/lib/python2.7/site-packages')
+sys.path.append('/home/damian/anaconda3/envs/aimsun_flow/lib/python2.7/site-packages')
 import numpy as np
 from aimsun_props import Aimsun_Params, Export_Params
 
-ap = Aimsun_Params("/home/cjrsantos/sa_flow/flow/flow/utils/aimsun/aimsun_props.csv")
+ap = Aimsun_Params("/home/damian/sa_flow/flow/flow/utils/aimsun/aimsun_props.csv")
 green_phases = {}
 model = gk.GKSystem.getSystem().getActiveModel()
 
