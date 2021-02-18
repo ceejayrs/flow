3b77db893a4b8d36ac0983485114789824a297be
Ceejay Santos <cjrsantos529@gmail.com>
2021-02-19 07:41:53 +0800

    added lstm

--- flow/utils/aimsun/run.py
+++ flow/utils/aimsun/run.py
@@ -18,7 +18,7 @@ from PyANGKernel import *  # noqa
 import AAPI as aimsun_api  # noqa
 from aimsun_props import Aimsun_Params, Export_Params
 
-ap = Aimsun_Params('/home/cjrsantos/sa_flow/flow/flow/utils/aimsun/aimsun_props.csv')
+ap = Aimsun_Params('/home/damian/sa_flow/flow/flow/utils/aimsun/aimsun_props.csv')
 interval = 600 #10 minutes
 
 ## Export files
